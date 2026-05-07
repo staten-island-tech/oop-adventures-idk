75437caf7c20a0440683ab5bcb9d138f3fc6da35
DanielC966 <danielc966@nycstudents.net>
2026-05-07 08:56:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("hello guys")
+import random
+
+class Player:
+    def __init__(self, name, health, strength, defense, hunger, inventory):
+        self.name = name
+        self.health = health
+        self.strength = strength
+        self.defense = defense
+        self.hunger = hunger
+        self.inventory = inventory
+    def checkStats(self):
+        print(f"Name: {self.name}")
+        print(f"Health: {self.health}")
+        print(f"Strength: {self.strength}")
+        print(f"Defense: {self.defense}")
+        print(f"Hunger: {self.hunger}")
+        print(f"Inventory: {self.inventory}")
+pName = input("What do you want the player's name to be?\n*Stats will be randomized\n- ")
+
+player = Player(pName, 100, random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), ['empty'] )
+
+player.checkStats()
